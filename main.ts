7d64cc95fe620cf41286841fc9cdc50a8ea673cd
Connor-Wilkins <159037610+SchoolAccount2123@users.noreply.github.com>
2025-11-19 19:07:46 +0000

    finished code commiting

---
+++ main.ts
@@ -0,0 +1,17 @@
+// this code will add 3 numbers together and find the sum of them
+
+
+// this area creates the number variables
+
+const num1: number = 56.9;
+const num2: number = 89.7;
+const num3: number = 90.2;
+
+// this part will calculate the sum and then the avarage
+
+const sum: number = num1 + num2 + num3;
+const avarage: number = sum / 3;
+
+//displaying the resutlts
+console.log("The numbers are:", num1, num2, num3);
+console.log("The avarage of those numbers are:", avarage)
